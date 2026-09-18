721cb3d00c3ec0c31f1f46327260ca98f0416ddb
Ngô Thanh Nguyên <ntn5678212@gmail.com>
2026-09-18 15:50:07 +0700

    Code MainActivity.kt va hoan thien bai tap thuc hanh

--- app/src/main/java/com/example/studentprofilecard/MainActivity.kt
+++ app/src/main/java/com/example/studentprofilecard/MainActivity.kt
@@ -1,20 +1,82 @@
 package com.example.studentprofilecard
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.studentprofilecard.databinding.ActivityMainBinding
+import com.example.studentprofilecard.model.Student
+import com.example.studentprofilecard.utils.toAcademicRanking
+import com.example.studentprofilecard.utils.toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var currentStudent = Student(
+        id = "2415053122330",
+        name = "Ngo Thanh Nguyen",
+        className = "126LTDD02",
+        email = "2415053122330@sv.ute.udn.vn",
+        gpa = 3.8
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        bindStudentData(currentStudent)
+
+        binding.btnUpdateGpa.setOnClickListener {
+
+            val inputStr =
+                binding.edtNewGpa.text.toString().trim()
+
+            val newGpa =
+                inputStr.toDoubleOrNull()
+
+
+            if (newGpa == null || newGpa !in 0.0..4.0) {
+
+                binding.edtNewGpa.error =
+                    "Vui lòng nhập GPA hợp lệ (0.0 - 4.0)"
+
+                toast("Điểm GPA không hợp lệ!")
+
+                return@setOnClickListener
+            }
+
+
+            currentStudent =
+                currentStudent.copy(
+                    gpa = newGpa
+                )
+
+            bindStudentData(currentStudent)
+
+
+            toast("Cập nhật điểm thành công!")
+        }
+    }
+
+
+    private fun bindStudentData(student: Student) {
+
+        with(binding) {
+
+            tvName.text =
+                student.name
+
+            tvStudentId.text =
+                "MSSV: ${student.id} • Lớp: ${student.className}"
+
+            tvGpaBadge.text =
+                "${student.gpa} GPA (${student.gpa.toAcademicRanking()})"
+
+            edtNewGpa.setText(
+                student.gpa.toString()
+            )
         }
     }
 }
